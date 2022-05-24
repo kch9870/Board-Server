e88302e1c1555220f03d4ea38f8c093a76c3a97e
simon_kong <simon.kong@detion.me>
2022-05-24 23:19:16 +0900

    ADD 1. 테스트 가능한 url추가 ( /test )

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express11' });
 });
 
+router.get('/test', function(req, res, next) {
+  res.send(
+    {
+      test1: "test1",
+      test2: "test2",
+      test3: {
+        test: "test",
+        test1: "test"
+      }
+    });
+});
+
 module.exports = router;
