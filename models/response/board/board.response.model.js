77e64e5b0de6eef9c8a8460309f76b53b256b583
DESKTOP-3NVTF33\KONGS-LAPTOP <boipqiod@naver.com>
2022-06-26 21:46:28 +0900

    FIX 1. 게시판 리스트 불러온 후 저장 로직 수정

--- models/response/board/board.response.model.js
+++ models/response/board/board.response.model.js
@@ -14,20 +14,11 @@ class BoardResponseModel extends BaseResponseModel{
         this.totalCount = addResult.totalCount
         this.lastPage = lastPage
 
-        for(let i = 0; i < addResult.board.length; i++){
-            const board = {}       // 초기화
-    
-            board["boardId"] = addResult.board[i]["board_id"]
-            board["title"] = addResult.board[i]["title"]
-            board["category"] = addResult.board[i]["category"]
-            board["date"] = addResult.board[i]["date"]
-            board["views"] = addResult.board[i]["views"]
-            board["nickName"] = addResult.board[i]["nick_name"]
-            board["commentCount"] = addResult.board[i]["commentCount"]
-    
-            this.boardList.push(board)
+        for(const item of addResult.board){
+            for(const key in item){
+                this.boardList[key] = item[key]
+            }
         }
-
     }
 
 }
