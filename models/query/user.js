4c9617d62cd44a332c14777bfd9320755cf60ea6
DESKTOP-3NVTF33\KONGS-LAPTOP <boipqiod@naver.com>
2022-06-26 22:15:24 +0900

    FIX 1. 오타 이슈 수정

--- models/query/user.js
+++ models/query/user.js
@@ -21,7 +21,7 @@ async function getUserInfo(columnName ,value, targetColumn="*"){
     if(!value) return false
 
     switch (targetColumn){
-        case '*': targetColumn = 'user_id as userID, email, name, nick_name as nickName, password'
+        case '*': targetColumn = 'user_id as userId, email, name, nick_name as nickName, password'
             break
         case 'user_id' : targetColumn = 'user_id as userID'
             break
