e85a5140763fafcd346014652a46e777eeb8ab05
chaewonKim <kch9870@naver.com>
2022-06-30 20:45:44 +0900

    Revert "FIX - 게시판 상세정보 불러오기 오류 해결"
    This reverts commit 21605547979d05a310e34a25aa3a1c20dc7529b5.

--- models/response/board/boardDetail.response.model.js
+++ models/response/board/boardDetail.response.model.js
@@ -14,17 +14,17 @@ class BoardDetailResponseModel extends BaseResponseModel{
 
         this.title = boardDetailResult["title"]
         this.content = boardDetailResult["content"]
-        this.nickName = boardDetailResult["nickName"]
+        this.nickName = boardDetailResult["nick_name"]
         this.category = boardDetailResult["category"]
         this.date = boardDetailResult["date"]
  
         for(let i = 0; i < boardDetailResult.comment.length; i++){
             const comment = {}       // 초기화
     
-            comment["commentId"] = boardDetailResult.comment[i]["commentId"]
-            comment["nickName"] = boardDetailResult.comment[i]["nickName"]
+            comment["commentId"] = boardDetailResult.comment[i]["comment_id"]
+            comment["nickName"] = boardDetailResult.comment[i]["nick_name"]
             comment["date"] = boardDetailResult.comment[i]["date"]
-            comment["comment"] = boardDetailResult.comment[i]["comment"]
+            comment["comments"] = boardDetailResult.comment[i]["comments"]
     
             this.commentList.push(comment)
         }
